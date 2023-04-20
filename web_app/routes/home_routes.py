f80bf33378848d71435fd70fff94edf5a6340138
mp1860 <128503775+mp1860@users.noreply.github.com>
2023-04-20 11:39:03 -0400

    changes

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -27,5 +27,5 @@ def hello_world():
     name = url_params.get("name") or "World"
     message = f"Hello, {name}!"
 
-    return message
-    #return render_template("hello.html", message=message)
+    #return message
+    return render_template("hello.html", message=message, other='YEAH', x='5')
